a77e50be8999b226b1267897581da7d9e90d5582
Jorge Anibal Sardón <36904152+anibal317@users.noreply.github.com>
2026-06-16 14:06:47 -0300

    test5

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const btnSend = document.getElementById("btnSend")
 const result = document.getElementById("result")
 const code = document.getElementById("code")
-code.value="Saludos"
+code.value='<p style="color: red;">Saludos</p>'
 
 btnSend.addEventListener("click", () => {
-    result.innerText="TEST"
+    result.innerText=code.value
 })
