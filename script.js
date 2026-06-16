9987cabc56ab2545d6052aaba08c88678bffecbd
Jorge Anibal Sardón <36904152+anibal317@users.noreply.github.com>
2026-06-16 13:41:49 -0300

    connecting resiurces 2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const btnSend = document.getElementById("btnSend")
 const result = document.getElementById("result")
 const code = document.getElementById("code")
-
+alert("conectado")
 btnSend.addEventListener("click", () => {
     result.innerText("TEST")
 })
